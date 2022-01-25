cd3b217f053b8173bfb16bc35c3fb381621d5c73
Denis <darksaude161190@yandex.ru>
2022-01-25 21:24:06 +0300

    Шаблон

---
+++ src/js/script.js
@@ -0,0 +1,23 @@
+/* <------ Подключение js ------> */
+
+import './libs/nav/_nav';
+
+/* <------ Подключение font awesome ------> */
+
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+/* <------ Подключение scss ------> */
+
+import '../scss/style.scss';
+
+/* <------ Подключение pug ------> */
+
+import '../index.pug';
+import '../pug/pages/about.pug';
+import '../pug/component/_head.pug';
+import '../pug/component/_header.pug';
+import '../pug/component/_footer.pug';
+import '../pug/component/_nav.pug';
+import '../pug/layout/_sample.pug';
+
